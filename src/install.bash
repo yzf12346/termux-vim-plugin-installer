8a0fa20821853205ce0c6072a2bdfa5712aa52b9
yzf12346 <76731359+yzf12346@users.noreply.github.com>
2022-09-08 23:23:15 +0800

    Update install.bash

--- src/install.bash
+++ src/install.bash
@@ -3,6 +3,8 @@
  sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.bfsu.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list &&sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.bfsu.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list &&sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.bfsu.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list &&apt update -y && apt upgrade -y
 
 # 设置键位
+mkdir -p ~/.termux/
+touch  ~/.termux/termux.properties
 echo "extra-keys = [[{key:'ESC',popup:'\"'},{key:'TAB',popup:'\`'},{key:'CTRL',popup:'ALT'},{key:'SHIFT',popup:'='},{key:'<',popup:'>'},'UP','ENTER'],[{key:'{',popup:'}'},{key:'(',popup:')'},{key:'[',popup:']'},{key:'$',popup:'|'},'LEFT','DOWN','RIGHT']]" >> ~/.termux/termux.properties
 termux-reload-settings
 
